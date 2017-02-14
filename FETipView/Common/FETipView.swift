fe7097fc74ba63c712cfae9be0a2ee8dc5540e8f
FlyElephant <keso_xiang@talkmate.com>
2017-02-14 17:20:19 +0800

    箭头左右方向处理~

--- FETipView/Common/FETipView.swift
+++ FETipView/Common/FETipView.swift
@@ -84,7 +84,16 @@ class FETipView:UIView {
         point = preference.positioning.targetPoint
         arrowHeight = preference.drawing.arrowHeight
         arrowWidth = preference.drawing.arrowWidth
-        width = preference.drawing.maxTextWidth + preference.drawing.textInset * 2
+        
+        switch preference.positioning.arrowPosition {
+        case UIPopoverArrowDirection.up,UIPopoverArrowDirection.down:
+              width = preference.drawing.maxTextWidth + preference.drawing.textInset * 2
+        case UIPopoverArrowDirection.left,UIPopoverArrowDirection.right:
+            width = preference.drawing.maxTextWidth + preference.drawing.textInset * 2 + arrowWidth
+        default:
+            width = preference.drawing.maxTextWidth
+        }
+      
         
         message = preference.drawing.message
         
@@ -143,7 +152,6 @@ class FETipView:UIView {
         
         let minHeight:CGFloat = preference.drawing.minHeight
         
-        
         let retainMinHeight:CGFloat = minHeight - arrowHeight - textInset * 2
         let retainMaxHeight:CGFloat = preference.drawing.maxHeight - arrowHeight - textInset * 2
         
@@ -170,24 +178,40 @@ class FETipView:UIView {
     private func adjustFrame() {
         var frameX:CGFloat = point.x - width / 2
         var frameY:CGFloat = point.y
-        let textHInset:CGFloat = ceil(((contentSize.height - arrowHeight) - textSize.height) / 2)
+        var textHInset:CGFloat = ceil(((contentSize.height - arrowHeight) - textSize.height) / 2)
         
         var contentY:CGFloat = textHInset
         
-        if (point.x - width / 2) < 0 {
-            frameX = 1
-        }
-        
-        if (point.x + width / 2 > screenWidth) {
-            frameX = screenWidth - width - 1
-        }
-        
         switch preference.positioning.arrowPosition {
         case UIPopoverArrowDirection.up:
+            if (point.x - width / 2) < 0 {
+                frameX = 1
+            }
+            
+            if (point.x + width / 2 > screenWidth) {
+                frameX = screenWidth - width - 1
+            }
             contentY = arrowHeight + textHInset
             break
         case UIPopoverArrowDirection.down:
+            if (point.x - width / 2) < 0 {
+                frameX = 1
+            }
+            
+            if (point.x + width / 2 > screenWidth) {
+                frameX = screenWidth - width - 1
+            }
             frameY = point.y - contentSize.height
+            break
+        case UIPopoverArrowDirection.left:
+            frameX = point.x - width
+            frameY = point.y - contentSize.height / 2
+            contentY = preference.drawing.textInset
+            break
+        case UIPopoverArrowDirection.right:
+            frameX = point.x
+            frameY = point.y - contentSize.height / 2
+            contentY = preference.drawing.textInset
             break
         default:
             break
@@ -220,6 +244,13 @@ class FETipView:UIView {
             case UIPopoverArrowDirection.down:
              drawArrowDown(contourPath: contourPath)
             break
+            case UIPopoverArrowDirection.left:
+             drawArrowLeft(contourPath: contourPath)
+            break
+            case UIPopoverArrowDirection.right:
+            drawArrowRight(contourPath: contourPath)
+            break
+            
            default:
            break
         }
@@ -272,6 +303,52 @@ class FETipView:UIView {
         contourPath.addLine(to: CGPoint(x: beginX + arrowWidth / 2, y: height))
         
         contourPath.addLine(to: CGPoint(x: beginX, y: contentHeight))
+    }
+    
+    private func drawArrowLeft(contourPath:CGMutablePath) {
+        let contentHeight:CGFloat = contentSize.height
+        let radius:CGFloat = preference.drawing.cornerRadius
+        
+        let beginY:CGFloat = point.y - self.frame.origin.y
+        
+        
+        let pathWidth:CGFloat = width - arrowWidth
+        
+        contourPath.move(to: CGPoint(x: width, y: beginY))
+        
+        contourPath.addLine(to: CGPoint(x: pathWidth, y: beginY - arrowHeight / 2))
+        
+        contourPath.addArc(tangent1End:CGPoint(x: pathWidth, y: 0), tangent2End: CGPoint(x: 0, y: 0), radius: radius)
+        contourPath.addArc(tangent1End:CGPoint(x: 0, y: 0), tangent2End: CGPoint(x: 0, y: contentHeight), radius: radius)
+        
+        contourPath.addArc(tangent1End:CGPoint(x: 0, y: contentHeight), tangent2End: CGPoint(x: pathWidth, y: contentHeight), radius: radius)
+        contourPath.addArc(tangent1End:CGPoint(x: pathWidth, y: contentHeight), tangent2End: CGPoint(x: pathWidth, y: 0), radius: radius)
+        
+        contourPath.addLine(to: CGPoint(x: pathWidth, y: beginY + arrowHeight / 2))
+        
+        contourPath.addLine(to: CGPoint(x: width, y: beginY))
+    }
+    
+    
+    private func drawArrowRight(contourPath:CGMutablePath) {
+        let contentHeight:CGFloat = contentSize.height
+        let radius:CGFloat = preference.drawing.cornerRadius
+        
+        let beginY:CGFloat = point.y - self.frame.origin.y
+        
+        contourPath.move(to: CGPoint(x: 0, y: beginY))
+        
+        contourPath.addLine(to: CGPoint(x: arrowWidth, y: beginY - arrowHeight / 2))
+        
+        contourPath.addArc(tangent1End:CGPoint(x: arrowWidth, y: 0), tangent2End: CGPoint(x: width, y: 0), radius: radius)
+        contourPath.addArc(tangent1End:CGPoint(x: width, y: 0), tangent2End: CGPoint(x: width, y: contentHeight), radius: radius)
+        
+        contourPath.addArc(tangent1End:CGPoint(x: width, y: contentHeight), tangent2End: CGPoint(x: arrowWidth, y: contentHeight), radius: radius)
+        contourPath.addArc(tangent1End:CGPoint(x: arrowWidth, y: contentHeight), tangent2End: CGPoint(x: arrowWidth, y: 0), radius: radius)
+        
+        contourPath.addLine(to: CGPoint(x: arrowWidth, y: beginY + arrowHeight / 2))
+        
+        contourPath.addLine(to: CGPoint(x: 0, y: beginY))
     }
     
     
--- FETipView/ViewController.swift
+++ FETipView/ViewController.swift
@@ -79,5 +79,28 @@ class ViewController: UIViewController {
     }
     
 
+    @IBAction func leftAction(_ sender: UIButton) {
+        var preference:FEPreferences = FEPreferences()
+        preference.positioning.targetPoint = CGPoint(x: sender.frame.minX, y: sender.center.y)
+        preference.positioning.arrowPosition = UIPopoverArrowDirection.left
+        // preference.drawing.textBackGroundColor = UIColor.gray
+        preference.drawing.message = "《解忧杂货店》是日本作家东野圭吾写作的奇幻温情小说。2011年于《小说野性时代》连载，于2012年3月由角川书店发行单行本。该书讲述了在僻静街道旁的一家杂货店，只要写下烦恼投进店前门卷帘门的投信口，第二天就会在店后的牛奶箱里得到回答：因男友身患绝症，年轻女孩月兔在爱情与梦想间徘徊；松冈克郎为了音乐梦想离家漂泊，却在现实中寸步难行；少年浩介面临家庭巨变，挣扎在亲情与未来的迷茫中……他们将困惑写成信投进杂货店，奇妙的事情随即不断发生"
+        
+        let tipView:FETipView = FETipView(preferences: preference)
+        tipView.show()
+    }
+    
+    @IBAction func rightAction(_ sender: UIButton) {
+        var preference:FEPreferences = FEPreferences()
+        preference.positioning.targetPoint = CGPoint(x: sender.frame.maxX, y: sender.center.y)
+        preference.positioning.arrowPosition = UIPopoverArrowDirection.right
+
+        preference.drawing.message = "《解忧杂货店》是日本作家东野圭吾写作的奇幻温情小说。2011年于《小说野性时代》连载，于2012年3月由角川书店发行单行本。该书讲述了在僻静街道旁的一家杂货店，只要写下烦恼投进店前门卷帘门的投信口，第二天就会在店后的牛奶箱里得到回答：因男友身患绝症，年轻女孩月兔在爱情与梦想间徘徊；松冈克郎为了音乐梦想离家漂泊，却在现实中寸步难行；少年浩介面临家庭巨变，挣扎在亲情与未来的迷茫中……他们将困惑写成信投进杂货店，奇妙的事情随即不断发生"
+        
+        let tipView:FETipView = FETipView(preferences: preference)
+        tipView.show()
+    }
+    
+
 }
 
